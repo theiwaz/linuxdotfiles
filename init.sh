23bfdb90e41391ff8d162341f7d0ec21c0e73f8e
RvdW <ryan@familievanderwal.net>
2026-03-20 09:56:26 +0100

    fixes

--- init.sh
+++ init.sh
@@ -3,45 +3,55 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # Install utils
-sudo apt-get install zsh \
-  neovim \
-  ripgrep \
-  zoxide \
-  tmux \
-  fonts-powerline  \
-  lua5.4 luarocks \
-  python3-venv \ # for the python-lsp-server 
-  build-essential \ 
-  curl \
-  unzip \
-  git \
-  ripgrep
-
+echo ## ZSH
+sudo apt-get install zsh
+echo ## RipGrep
+sudo apt-get install ripgrep
+echo ## Zoxide
+sudo apt-get install zoxide
+echo ## TMUX
+sudo apt-get install tmux
+echo ## Powerline fonts
+sudo apt-get install fonts-powerline
+echo ## Firacode font
 sudo apt install fonts-firacode
-curl -L git.io/antigen > antigen.zsh
-chsh -s /usr/bin/zsh 
+echo ## LUA
+sudo apt-get install lua5.4 luarocks
+echo ## Python venv
+sudo apt-get install python3-venv
+echo ## Curl
+sudo apt-get install curl
+echo ## GIT
+sudo apt-get install git
+echo ## Build Essential
+sudo apt-get install build-essential
+echo ## UNZIP
+sudo apt-get install unzip
 
-# Download NeoVim, zshrc cotains adding to path declaration 
+curl -L https://git.io/antigen >antigen.zsh
+chsh -s /usr/bin/zsh
+
+# Download NeoVim, zshrc cotains adding to path declaration
 sudo curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2> /dev/null
-mkdir -p ~/.config 
+rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2>/dev/null
+mkdir -p ~/.config
 
 # Pluginmanager for vi.m
-curl  ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# TMUX plugin manager 
+# TMUX plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Create sim links for dotfile referencing them in my linuxdotfile directory
@@ -53,11 +63,6 @@ ln -s ~/linuxdotfiles/antigen.zsh ~/.antigen.zsh
 ln -s ~/linuxdotfiles/nvim ~/.config/nvim
 ln -s ~/linuxdotfiles/yazi ~/.config/yazi
 
-source ./install-yazi.sh
-source ./install-rust.sh
-source ./install-dotnet.sh
-
-
-
-
-
+sudo source ./install-yazi.sh
+sudo source ./install-rust.sh
+sudo source ./install-dotnet.sh
